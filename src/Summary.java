5457271f4d6d6df373d4a93f3fdc30d78aebbacb
SBarrera16 <79816580+SBarrera16@users.noreply.github.com>
2021-03-05 19:15:19 -0500

    Update Summary.java

--- src/Summary.java
+++ src/Summary.java
@@ -1,2 +1,8 @@
+import java.time.LocalDate;
+
 public class Summary {
+    private LocalDate DATE;
+    private int ActiveProjects;
+    private int OpenActivities;
+    private int ClosedActivities;
 }
